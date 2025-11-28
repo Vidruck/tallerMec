61b8650bc61f4082ec8f620b9d49399c49d395d9
Vidruck <alex_gohe@hotmail.com>
2025-11-27 18:26:57 -0600

    Taller Mecanico
    Proyecto de evaluación  de app web monolítica  para administración de un taller mecánico

---
+++ src/main/java/mx/ipn/upiicsa/dis/tallerMec/TallerMecanicoApplication.java
@@ -0,0 +1,13 @@
+package mx.ipn.upiicsa.dis.tallerMec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerMecanicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerMecanicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/ipn/upiicsa/dis/tallerMec/TallerMecanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ipn.upiicsa.dis.tallerMec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMecanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
